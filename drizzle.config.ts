07411099dc2217135ee17766371fb8667fbc17e4
Rahul S <rahulsenthil90@gmail.com>
2026-09-21 17:26:55 +0530

    Use environment variable for database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+
+config({ path: ".env" });
 
 export default defineConfig({
   schema: "./src/server/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgres://postgres:R@hu_3012@127.0.0.1:5432/Job_Portal",
+    url: process.env.DATABASE_URL!,
   },
 });
